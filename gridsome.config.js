e1c75afae3d8497ca5ecff9988c789958bcb5c48
YuYun95 <2219946651@qq.com>
2020-09-27 22:56:59 +0800

    frist commit

---
+++ gridsome.config.js
@@ -0,0 +1,35 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: 'Gridsome',
+  plugins: [
+    {
+      use: '@gridsome/source-strapi',
+      options: {
+        apiURL: process.env.GRIDSOME_API_URL,
+        queryLimit: 1000, // Defaults to 100
+        contentTypes: ['blog','project'], // StrapiPost  获取集合
+        // typeName: 'Strapi,
+        singleTypes: ['vblog'], // 获取单个数据
+        // Possibility to login with a Strapi user,
+        // when content types are not publicly available (optional).
+        // loginData: {
+        //   identifier: '',
+        //   password: ''
+        // }
+      }
+    }
+  ],
+  templates: {
+    StrapiBlog: [
+      {
+        path: '/blog-detail/:id',
+        component: './src/templates/Details.vue'
+      }
+    ]
+  }
+}
